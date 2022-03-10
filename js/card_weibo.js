0293d861bbd4d72e01a75bc7366a86d19cedda75
aCodeFish <1564505981@qq.com>
2022-03-10 16:20:18 +0800

    Site updated: 2022-03-10 16:19:59

--- js/card_weibo.js
+++ js/card_weibo.js
@@ -1,3 +1,4 @@
+// 引入微博组件
 fetch('https://weibo-top-api.vercel.app/api').then(data => data.json()).then(data => {
     let html = '<style>#weibo-container{overflow-y:auto;-ms-overflow-style:none;scrollbar-width:none}#weibo-container::-webkit-scrollbar{display:none}.weibo-list-item{display:flex;flex-direction:row;justify-content:space-between;flex-wrap:nowrap}.weibo-title{white-space:nowrap;overflow:hidden;text-overflow:ellipsis;margin-right:auto}.weibo-num{float:right}.weibo-hotness{display:inline-block;padding:0 6px;transform:scale(.8) translateX(-3px);color:#fff;border-radius:8px}.weibo-new{background:#ff3852}.weibo-hot{background:#ff9406}.weibo-recommend{background:#00b7ee}.weibo-adrecommend{background:#febd22}.weibo-friend{background:#8fc21e}.weibo-boom{background:#bd0000}.weibo-topic{background:#ff6f49}.weibo-topic-ad{background:#4dadff}.weibo-boil{background:#f86400}</style>'
     html += '<div class="weibo-list">'
@@ -9,12 +10,165 @@ fetch('https://weibo-top-api.vercel.app/api').then(data => data.json()).then(dat
         '荐': 'weibo-recommend'
     }
     for (let item of data) {
-        html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[(item.hot || '荐')] + '">' + (item.hot || '荐') + '</div>'
-            + '<span class="weibo-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank">' + item.title + '</a></span>'
-            + '<div class="weibo-num"><span>' + item.num + '</span></div></div>'
+        html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[(item.hot || '荐')] + '">' + (item.hot || '荐') + '</div>' +
+            '<span class="weibo-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank">' + item.title + '</a></span>' +
+            '<div class="weibo-num"><span>' + item.num + '</span></div></div>'
     }
     html += '</div>'
     document.getElementById('weibo-container').innerHTML = html
-}).catch(function (error) {
+}).catch(function(error) {
     console.log(error);
 });
+
+// 引入枫叶飘落效果
+var stop, staticx;
+var img = new Image();
+img.src = "https://img.cdn.nesxc.com/2022/02/202202251325420webp";
+
+function Sakura(x, y, s, r, fn) {
+    this.x = x;
+    this.y = y;
+    this.s = s;
+    this.r = r;
+    this.fn = fn
+}
+Sakura.prototype.draw = function(cxt) {
+    cxt.save();
+    var xc = 40 * this.s / 2;
+    cxt.translate(this.x, this.y);
+    cxt.rotate(this.r);
+    cxt.drawImage(img, 0, 0, 27 * this.s, 27 * this.s);
+    cxt.restore()
+};
+Sakura.prototype.update = function() {
+    this.x = this.fn.x(this.x, this.y);
+    this.y = this.fn.y(this.y, this.y);
+    this.r = this.fn.r(this.r);
+    if (this.x > window.innerWidth || this.x < 0 || this.y > window.innerHeight || this.y < 0) {
+        this.r = getRandom("fnr");
+        if (Math.random() > 0.4) {
+            this.x = getRandom("x");
+            this.y = 0;
+            this.s = getRandom("s");
+            this.r = getRandom("r")
+        } else {
+            this.x = window.innerWidth;
+            this.y = getRandom("y");
+            this.s = getRandom("s");
+            this.r = getRandom("r")
+        }
+    }
+};
+SakuraList = function() {
+    this.list = []
+};
+SakuraList.prototype.push = function(sakura) {
+    this.list.push(sakura)
+};
+SakuraList.prototype.update = function() {
+    for (var i = 0, len = this.list.length; i < len; i++) {
+        this.list[i].update()
+    }
+};
+SakuraList.prototype.draw = function(cxt) {
+    for (var i = 0, len = this.list.length; i < len; i++) {
+        this.list[i].draw(cxt)
+    }
+};
+SakuraList.prototype.get = function(i) {
+    return this.list[i]
+};
+SakuraList.prototype.size = function() {
+    return this.list.length
+};
+
+function getRandom(option) {
+    var ret, random;
+    switch (option) {
+        case "x":
+            ret = Math.random() * window.innerWidth;
+            break;
+        case "y":
+            ret = Math.random() * window.innerHeight;
+            break;
+        case "s":
+            ret = Math.random();
+            break;
+        case "r":
+            ret = Math.random() * 4;
+            break;
+        case "fnx":
+            random = -0.5 + Math.random() * 1;
+            ret = function(x, y) {
+                return x + 0.5 * random - 1.7
+            };
+            break;
+        case "fny":
+            random = 1.5 + Math.random() * 0.7;
+            ret = function(x, y) {
+                return y + random
+            };
+            break;
+        case "fnr":
+            random = Math.random() * 0.03;
+            ret = function(r) {
+                return r + random
+            };
+            break
+    }
+    return ret
+}
+
+function startSakura() {
+    requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame;
+    var canvas = document.createElement("canvas"),
+        cxt;
+    staticx = true;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.setAttribute("style", "position: fixed;left: 0;top: 0;pointer-events: none;");
+    canvas.setAttribute("id", "canvas_sakura");
+    document.getElementsByTagName("body")[0].appendChild(canvas);
+    cxt = canvas.getContext("2d");
+    var sakuraList = new SakuraList();
+    for (var i = 0; i < 50; i++) {
+        var sakura, randomX, randomY, randomS, randomR, randomFnx, randomFny;
+        randomX = getRandom("x");
+        randomY = getRandom("y");
+        randomR = getRandom("r");
+        randomS = getRandom("s");
+        randomFnx = getRandom("fnx");
+        randomFny = getRandom("fny");
+        randomFnR = getRandom("fnr");
+        sakura = new Sakura(randomX, randomY, randomS, randomR, {
+            x: randomFnx,
+            y: randomFny,
+            r: randomFnR
+        });
+        sakura.draw(cxt);
+        sakuraList.push(sakura)
+    }
+    stop = requestAnimationFrame(function() {
+        cxt.clearRect(0, 0, canvas.width, canvas.height);
+        sakuraList.update();
+        sakuraList.draw(cxt);
+        stop = requestAnimationFrame(arguments.callee)
+    })
+}
+window.onresize = function() {
+    var canvasSnow = document.getElementById("canvas_snow")
+};
+img.onload = function() {
+    startSakura()
+};
+
+function stopp() {
+    if (staticx) {
+        var child = document.getElementById("canvas_sakura");
+        child.parentNode.removeChild(child);
+        window.cancelAnimationFrame(stop);
+        staticx = false
+    } else {
+        startSakura()
+    }
+};
